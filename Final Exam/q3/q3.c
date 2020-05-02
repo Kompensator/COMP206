b9baa4c7b043c497a17291c3f4e09b8fdb198128
DY <23586296+Kompensator@users.noreply.github.com>
2020-05-02 09:03:48 -0400

    Final edits for q3

--- Final Exam/q3/q3.c
+++ Final Exam/q3/q3.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 char *getInfo() {
-   char buffer[1000];
-   scanf("%s", buffer);
+   char buffer[1000] = "12345";
+   // char buffer[1000];
+   // scanf("%s", buffer);
    return buffer;
 }
 int main() {
    char *ptr;
    ptr = getInfo();
-   printf("%s\n", ptr); // <--- error
+   for (int i = 0; i < 5; i++) putc(*(ptr+i), stdout);
+   // printf("%s\n", ptr); // <--- error
    return 0;
 }
